e24c46ccc4b60f75fe5608d861c9adc48ed6667b
janco-arch <116068842+janco-arch@users.noreply.github.com>
2025-04-25 09:34:03 +0300

    Update e-commerce.sql

--- e-commerce.sql
+++ e-commerce.sql
@@ -9,6 +9,13 @@ brand_id int primary key,
 brand_name varchar(60) not null,
 brand_description varchar(255));
 
+-- insert values into brand table
+INSERT INTO brand (brand_id, brand_name,brand_description) VALUES
+(1, 'Nike',"Renowned for sportswear and athletic footwear"),
+(2, 'Apple',"A technology giant known for groundbreaking electronics such as iPhones"),
+(3, 'Sony',"A leading brand in consumer electronics, especially known for entertainment products like PlayStation consoles"),
+(4, 'Adidas',"A global sportswear company specializing in performance-driven footwear");
+
 
 -- product table
 create table product(
@@ -18,13 +25,29 @@ product_price decimal(10,2) not null,
 brand_id int not null,
 foreign key (brand_id) references brand(brand_id));
 
+-- insert into product table
+ insert into product(product_id,product_name,product_price,brand_id)
+ values
+ (1,"Running Shoes",12000.00,1),
+ (2,"Iphone 15",145000.45,2),
+ (3,"Play Station",60000.20,3),
+ (4,"Football",23523.45,4);
 
--- product _image table
+
+-- product_image table
 create table product_image(
 image_id int primary key,
 product_id  int not null,
 image_url varchar(255) not null,
 foreign key (product_id) references product(product_id));
+
+-- insert into product_image table
+insert into product_image (image_id, product_id, image_url)
+values
+(1, 1, 'https://example.com/nike-shoes.jpg'),
+(2, 2, 'https://example.com/iphone-14.jpg'),
+(3, 3, 'https://example.com/ps5.jpg'),
+(4, 4, 'https://example.com/soccer-ball.jpg');
 
 
 -- product_item table
@@ -34,6 +57,13 @@ product_id int not null,
 variation_id int not null,
 stock_quantity int not null,
 foreign key (product_id) references product(product_id));
+
+-- isert values into product_item table
+insert into product_item (item_id, product_id, variation_id,stock_quantity) 
+values
+(1,1,1,50),
+(2,2,2,30),
+(3, 3,3,100);
 
 
 -- product _attribute table
@@ -46,29 +76,7 @@ category_id int not null,
 attribute_name varchar(60),
 foreign key (product_id) references product(product_id));
 
-
-
- insert into product(product_id,product_name,product_price,brand_id)
- values
- (1,"Running Shoes",12000.00,1),
- (2,"Iphone 15",145000.45,2),
- (3,"Play Station",60000.20,3),
- (4,"Football",23523.45,4);-- 
-
-
-insert into product_image (image_id, product_id, image_url)
-values
-(1, 1, 'https://example.com/nike-shoes.jpg'),
-(2, 2, 'https://example.com/iphone-14.jpg'),
-(3, 3, 'https://example.com/ps5.jpg'),
-(4, 4, 'https://example.com/soccer-ball.jpg');
-
-insert into product_item (item_id, product_id, variation_id,stock_quantity) 
-values
-(1,1,1,50),
-(2,2,2,30),
-(3, 3,3,100);
-
+-- insert values into product_attribute table
 insert into product_attribute (attribute_id, product_id, attribute_type_id, attribute_value,category_id,attribute_name) 
 values
 (1, 1, 2, 'Mesh',1,"build material"),
